97f748dc8efda840788cca589e9e09771859d1f7
rileyseefeldt <rileyseefeldt@gmail.com>
2026-04-27 15:17:40 -0700

    get rid of unneded file

--- angr/utils/concrete.py
+++ angr/utils/concrete.py
@@ -13,11 +13,11 @@ def setup_concrete_hooks(project):
 
     Hooks the following symbols (when present and not already hooked):
 
-    * ``__libc_start_main`` -- replaced by :class:`~angr.procedures.glibc.concrete_libc_start_main.ConcreteLibcStartMain`
+    * ``__libc_start_main`` -- standard angr SimProcedure
     * ``exit`` / ``_exit`` / ``_Exit`` -- standard exit procedure
     * ``__ctype_b_loc``, ``__ctype_tolower_loc``, ``__ctype_toupper_loc`` -- locale tables
     """
-    from angr.procedures.glibc.concrete_libc_start_main import ConcreteLibcStartMain
+    from angr.procedures.glibc.__libc_start_main import __libc_start_main as LibcStartMain  # noqa: N812
     from angr.procedures.libc.exit import exit as ExitProcedure
     from angr.procedures.glibc.__ctype_b_loc import __ctype_b_loc  # noqa: N812
     from angr.procedures.glibc.__ctype_tolower_loc import __ctype_tolower_loc  # noqa: N812
@@ -25,7 +25,7 @@ def setup_concrete_hooks(project):
 
     sym = project.loader.find_symbol("__libc_start_main")
     if sym is not None and sym.rebased_addr not in project._sim_procedures:
-        project.hook(sym.rebased_addr, ConcreteLibcStartMain())
+        project.hook(sym.rebased_addr, LibcStartMain())
 
     for name in ("exit", "_exit", "_Exit"):
         sym = project.loader.find_symbol(name)
